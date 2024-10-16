e490eafd27f4e56bfdfd867b776ffcac7ce25002
Kalotaibi2 <kalotaibi@oakland.edu>
2024-10-16 14:34:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title('News Popularity Predictor')
+st.title('News Popularity Predictorsss')
 
 # User input for model selection
 model_choice = st.selectbox('Choose a model', ['Naive Bayes', 'Logistic Regression'])
