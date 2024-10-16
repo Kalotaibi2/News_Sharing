c2d13a9fbb2d311eac7d056d3f0f93d7d5bd7719
Kalotaibi2 <kalotaibi@oakland.edu>
2024-10-16 14:32:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title('News Popularity Predictor')
+
+# User input for model selection
+model_choice = st.selectbox('Choose a model', ['Naive Bayes', 'Logistic Regression'])
+
+# Add a button for prediction
+if st.button('Predict'):
+    st.write(f'Using {model_choice} model for prediction...')
